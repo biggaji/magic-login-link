58ae08403fa8b6e9a812ec67bfa04f50bbe89e29
biggaji <tobiajibade2017@yandex.com>
2023-12-23 01:33:45 +0100

    Added prettier config

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { sleep } from "bun";
-import figlet from "figlet";
+import { sleep } from 'bun';
+import figlet from 'figlet';
 
-console.log("Magic link implementation");
-const body = figlet.textSync("Magic link bun server");
+console.log('Magic link implementation');
+const body = figlet.textSync('Magic link bun server');
 
 // Set timeout to 2s
 await sleep(2000);

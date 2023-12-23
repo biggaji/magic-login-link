b9c5eaf50bd9372e832b97457f80286d0b765f87
biggaji <tobiajibade2017@yandex.com>
2023-12-23 01:30:03 +0100

    added more deps

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { sleep } from "bun";
 import figlet from "figlet";
 
-console.log("Hello via Bun!");
-const body = figlet.textSync('Bun');
+console.log("Magic link implementation");
+const body = figlet.textSync("Magic link bun server");
+
+// Set timeout to 2s
 await sleep(2000);
-console.log(body)
+console.log(body);
